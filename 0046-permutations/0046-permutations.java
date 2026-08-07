c2b5d182192b8e94cabe8a998cc559c565b3ccce
Shubh Ujala <166637739+Shubh-ujala@users.noreply.github.com>
2026-08-07 22:35:00 +0530

    Time: 0 ms (100%), Space: 45.6 MB (46.25%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,39 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        // without backtracking
 
-        List<List<Integer>> res = new ArrayList<>();
-        solve(nums,new ArrayList<>(),res);
+    List<List<Integer>> result = new ArrayList<>();
+    int n;
 
-        return res;
-    }
-    public void solve(int[] nums, List<Integer> curr, List<List<Integer>> res){
-        if(curr.size() == nums.length){
-            res.add(curr);
+    public void solve(int idx, int[] nums) {
+
+        if (idx == n) {
+            List<Integer> temp = new ArrayList<>();
+            for (int num : nums) {
+                temp.add(num);
+            }
+            result.add(temp);
             return;
         }
 
-        for(int num : nums){
-            if(curr.contains(num)) continue;
-            List<Integer> next = new ArrayList<>(curr);
-            next.add(num);
+        for (int i = idx; i < n; i++) {
+            swap(nums, i, idx);
 
-            solve(nums, next, res);
+            solve(idx + 1, nums);
+
+            swap(nums, i, idx);
         }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        n = nums.length;
+
+        solve(0, nums);
+
+        return result;
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
