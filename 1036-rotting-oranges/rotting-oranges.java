f4d4accdc6ea0c3e24238ca8d20d75fcadd6cfe3
Shubh Ujala <166637739+Shubh-ujala@users.noreply.github.com>
2026-08-11 19:13:40 +0530

    Time: 2 ms (89.77%) | Memory: 45 MB (5.52%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int freshCount = 0;
+        Queue<int[]> q = new LinkedList<>();
+
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ;j<m ; j++){
+                if(grid[i][j] == 1) freshCount++;
+                else if(grid[i][j] == 2) q.offer(new int[]{i,j});
+            }
+        }
+
+        if(freshCount == 0) return 0;
+        int time = 0;
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0 ; i<size; i++){
+                int rottenLocation[] = q.poll();
+                int r = rottenLocation[0];
+                int c = rottenLocation[1];
+
+                int neighbours[][] = {
+                    {r-1,c},{r,c+1},{r+1,c},{r,c-1}
+                };
+
+                for(int nbr[] : neighbours){
+                    int nr = nbr[0];
+                    int nc = nbr[1];
+
+                    // check if outof bound / already rotten(visited)
+
+                    if(nr<0 || nr >= n || nc < 0 || nc >= m || grid[nr][nc] == 2 || grid[nr][nc] == 0) continue;
+
+                    q.offer(new int[]{nr,nc});
+                    grid[nr][nc] = 2;
+                }
+            }
+
+            time++;
+            freshCount-= q.size();
+            if(freshCount == 0) break;
+        }
+        return freshCount == 0 ? time : -1;
+    }
+}
