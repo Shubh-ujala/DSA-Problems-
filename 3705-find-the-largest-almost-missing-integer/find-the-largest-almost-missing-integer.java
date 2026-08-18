1ff925b2f3f017b1eaa80f5afb9b6ab6f87bd514
Shubh Ujala <166637739+Shubh-ujala@users.noreply.github.com>
2026-08-18 19:21:47 +0530

    Time: 3 ms (63.51%) | Memory: 45.2 MB (53.33%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -2,39 +2,41 @@ class Solution {
     public int largestInteger(int[] nums, int k) {
         int n = nums.length;
         int ans = 0;
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
 
-        for(int i : nums){
-            hm.put(i, hm.getOrDefault(i,0)+1);
+        for (int i : nums) {
+            hm.put(i, hm.getOrDefault(i, 0) + 1);
         }
 
         int res = 0;
-        if(k == 1){
+        if (k == 1) {
             int temp = Integer.MIN_VALUE;
-            for(int key : hm.keySet()){
-                if(hm.get(key) == 1 && key > temp){
+            for (int key : hm.keySet()) {
+                if (hm.get(key) == 1 && key > temp) {
                     temp = key;
                 }
             }
 
             return temp == Integer.MIN_VALUE ? -1 : temp;
-        }else if(k == n){
+        } else if (k == n) {
             int max_i = Integer.MIN_VALUE;
-            for(int i : nums){
-                max_i = Math.max(max_i,i);
+            for (int i : nums) {
+                max_i = Math.max(max_i, i);
             }
 
             return max_i;
-        }else{
-            if(hm.get(nums[0]) == 1 && hm.get(nums[n-1]) == 1){
-                return Math.max(nums[0],nums[n-1]);
-            }else if(hm.get(nums[0]) != 1 && hm.get(nums[n-1]) == 1){
-                return nums[n-1];
-            }else if(hm.get(nums[0]) == 1 && hm.get(nums[n-1]) != 1){
-                return nums[0];
-            }else{
-                return -1;
+        } else {
+            int temp = -1;
+
+            if (hm.get(nums[0]) == 1) {
+                temp = Math.max(temp, nums[0]);
             }
+
+            if (hm.get(nums[n - 1]) == 1) {
+                temp = Math.max(temp, nums[n - 1]);
+            }
+
+            return temp;
         }
     }
 }
