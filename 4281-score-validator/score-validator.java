e39094490c9682a2fe3abd28383c1b8348f015a0
Shubh Ujala <166637739+Shubh-ujala@users.noreply.github.com>
2026-08-20 19:18:36 +0530

    Time: 3 ms (24.21%) | Memory: 47.2 MB (68.42%) - LeetSync

--- 4281-score-validator/score-validator.java
+++ 4281-score-validator/score-validator.java
@@ -4,6 +4,7 @@ class Solution {
         int counter = 0;
 
         for(String st : events){
+            if(counter == 10) break;
             if(st.equals("0") || st.equals("1") || st.equals("2") || st.equals("3") || st.equals("4") || st.equals("6")){
                 int num = Integer.valueOf(st);
                 score+= num;
@@ -13,7 +14,6 @@ class Solution {
                 score+=1;
             }
 
-            if(counter == 10) break;
         }
 
         return new int[]{score,counter};
