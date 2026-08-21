6ae01ce156902bdff48c10497822c481a3b4e80c
Shubh Ujala <166637739+Shubh-ujala@users.noreply.github.com>
2026-08-21 18:24:06 +0530

    Time: 3 ms (80.73%) | Memory: 43.4 MB (67.46%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,29 +1,24 @@
-class Solution {
-    public int calPoints(String[] operations) {
-        Stack<Integer> st = new Stack<>();
-
-        for(String s : operations){
-            if(s.equals("+")){
-                int a = st.pop();
-                int b = st.peek();
-                int sum = a+ b;
-
-                st.push(a);
-                st.push(sum);
-            }else if(s.equals("D")){
-                int a = st.peek();
-                st.push(2*a);
-            }else if(s.equals("C")){
-                st.pop();
-            }else{
-                // only digits are left
-                st.push(Integer.parseInt(s));
+public class Solution {
+    public int calPoints(String[] ops) {
+        int res = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (String op : ops) {
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newTop = top + stack.peek();
+                stack.push(top);
+                stack.push(newTop);
+                res += newTop;
+            } else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+                res += stack.peek();
+            } else if (op.equals("C")) {
+                res -= stack.pop();
+            } else {
+                stack.push(Integer.parseInt(op));
+                res += stack.peek();
             }
         }
-
-        int sum = 0;
-        while(!st.isEmpty()) sum+=st.pop();
-
-        return sum;
+        return res;
     }
 }
