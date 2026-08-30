4174e0aaae8a857e807d07f3f386215d7375a642
Shubh Ujala <166637739+Shubh-ujala@users.noreply.github.com>
2026-08-30 19:52:56 +0530

    Time: 1 ms (17.14%) | Memory: 45 MB (11.21%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,22 +1,26 @@
 class Solution {
     int[][] dp = new int[101][101];
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        for(int row[] : dp)Arrays.fill(row,-1);
+        
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        return solve(m - 1, n - 1,obstacleGrid);
-    }
 
-    public int solve(int i , int j , int[][] matrix){
+        for(int i = 0 ; i<m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if(obstacleGrid[i][j] == 1) continue;
 
-        if( i < 0 || j < 0) return 0;
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else {
+                    int up = 0;
+                    int left = 0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
 
-        if( matrix[i][j] == 1) return 0;
+                    dp[i][j] = up + left;
+                }
+            }
+        }
 
-        if( i == 0 && j == 0) return 1;
-
-        if( dp[i][j] != -1) return dp[i][j];
-
-        return dp[i][j] = solve(i - 1,j,matrix) + solve( i , j - 1 , matrix);
+        return dp[m-1][n-1];
     }
 }
