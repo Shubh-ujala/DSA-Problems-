55b07683597c47360cd9b7e8ff2216e2241a3124
Shubh Ujala <166637739+Shubh-ujala@users.noreply.github.com>
2026-09-06 15:59:36 +0530

    Time: 14 ms (70.83%) | Memory: 48.7 MB (81.70%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,44 +1,52 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
+
         int n = mat.length;
         int m = mat[0].length;
-        boolean visited[][] = new boolean[n][m];
+
+        int[][] dist = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+
         Queue<int[]> q = new LinkedList<>();
 
-        for(int i = 0 ; i<n ; i++){
-            for(int j = 0 ; j<m ; j++){
-                if(mat[i][j] == 0){
-                    q.offer(new int[]{i,j});
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (mat[i][j] == 0) {
+                    q.add(new int[]{i, j});
                     visited[i][j] = true;
                 }
             }
         }
 
-        bfs(visited,q,mat);
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
 
-        return mat;
-    }
-    private void bfs( boolean[][] visited, Queue<int[]> q, int[][] mat){
+        while (!q.isEmpty()) {
 
-        int[] dr = {-1,0,1,0};
-        int[] dc = {0,1,0,-1};
+            int[] cell = q.poll();
 
-        while(!q.isEmpty()){
-            int[] curr = q.poll();
+            int row = cell[0];
+            int col = cell[1];
 
-            int currRow = curr[0];
-            int currCol = curr[1];
+            for (int i = 0; i < 4; i++) {
 
-            for(int i = 0 ; i<4 ; i++){
-                int nr = currRow + dr[i];
-                int nc = currCol + dc[i];
+                int nrow = row + dr[i];
+                int ncol = col + dc[i];
 
-                if(nr >=0 && nr < mat.length && nc >= 0 && nc < mat[0].length && !visited[nr][nc]){
-                    visited[nr][nc] = true;
-                    mat[nr][nc] = mat[currRow][currCol] + 1;
-                    q.offer(new int[]{nr,nc});
+                if (nrow >= 0 && nrow < n &&
+                    ncol >= 0 && ncol < m &&
+                    !visited[nrow][ncol]) {
+
+                    visited[nrow][ncol] = true;
+
+                    dist[nrow][ncol] = dist[row][col] + 1;
+
+                    q.add(new int[]{nrow, ncol});
                 }
             }
         }
+
+        return dist;
     }
 }
